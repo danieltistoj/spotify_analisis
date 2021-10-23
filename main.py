deb8b8b3a986041cb742c7adab301ad63024af45
danieltistoj <jose.tisrey@gmail.com>
2021-10-23 15:13:27 -0600

    inicio

---
+++ main.py
@@ -0,0 +1,11 @@
+baseURL = 'https://spotifycharts.com/regional/'
+countryList = ['cl', 'co', 'ar', 'pe', 'pr', 'uy', 've', 'ec', 'pa', 'mx', 'hn', 'gt', 'cr', 'do', 'es']
+countryList = ['ec', 'pa', 'mx', 'hn', 'gt', 'cr', 'do', 'es']
+
+class SpotifyScrapper:
+    def requestAndObtainTopSongs(self, country: str, date: str, driver) -> Iterator[Song]:
+        pass
+
+
+if __name__ == '__main__':
+    print("Pycharm")
