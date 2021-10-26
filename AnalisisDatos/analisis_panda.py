422548ae954708e825f91e8fa191dc94e8228e10
danieltistoj <jose.tisrey@gmail.com>
2021-10-26 15:59:38 -0600

    parametro a ranking

--- AnalisisDatos/analisis_panda.py
+++ AnalisisDatos/analisis_panda.py
@@ -2,13 +2,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 class analis_datos:
-    def ranking10Artistas2017_2021(self):
+    #la funcion recibe el valor al que se quiere encotrar el ranking
+    def ranking102017_2021(self,valor):
         #Hacemo el dataframe
         df = pd.read_csv("../Respaldo_db/csv/cancion123459data.csv", index_col="_id")
         #Filtamos los datos, para eliminar los posibles valores nulos
         df_filtrado = df.fillna({"ranking": 0, "nombre": "", "artista": "", "pais": "", "data": ""})
         #Agrupamos por artista y hacemos un promedio del ranking por artista
-        df_agrupado = df_filtrado.groupby("artista").mean()
+        df_agrupado = df_filtrado.groupby(valor).mean()
         #Dejamos en el dataframe los artista que esta solo en el top 10
         df_agrupado2 = df_agrupado[df_agrupado["ranking"] <= 10]
        #Hacemos una grafica de barras horizontal. si quiere una grafica de barras vertical poner solo bar
@@ -18,8 +19,8 @@ class analis_datos:
         #Agregamos titulo de x
         plt.xlabel("Ranking")
         #Agregamos titulo de y
-        plt.ylabel("Artistas")
+        plt.ylabel(valor)
         #Pintamos
         plt.show()
 analisis = analis_datos()
-analisis.ranking10Artistas2017_2021()
+analisis.ranking102017_2021("nombre")
