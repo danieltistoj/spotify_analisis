95c16355994dd9877505c53bcdb8905973970116
danieltistoj <jose.tisrey@gmail.com>
2021-10-26 19:07:41 -0600

    grafica de barras ranking por pais del año 2017 al 2021

--- AnalisisDatos/analisis_panda.py
+++ AnalisisDatos/analisis_panda.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 class analis_datos:
     #la funcion recibe el valor al que se quiere encotrar el ranking
-    def ranking102017_2021(self,valor):
+    def ranking10_2017_2021(self,valor):
         #Hacemo el dataframe
         df = pd.read_csv("../Respaldo_db/csv/cancion123459data.csv", index_col="_id")
         #Filtamos los datos, para eliminar los posibles valores nulos
@@ -22,5 +22,53 @@ class analis_datos:
         plt.ylabel(valor)
         #Pintamos
         plt.show()
+    #el pais tiene que ser la clave como esta en la lista de main
+    def ranking10_2017_2021PorPais(self,pais,valor):
+        # Hacemo el dataframe
+        df = pd.read_csv("../Respaldo_db/csv/cancion123459data.csv", index_col="_id")
+        # Filtamos los datos, para eliminar los posibles valores nulos
+        df_filtrado = df.fillna({"ranking": 0, "nombre": "", "artista": "", "pais": "", "data": ""})
+        #obtenemos solo los registros que tengan el pais que recibimos
+        df_filtrado = df_filtrado[df_filtrado["pais"]==pais]
+        #Agrupamos
+        df_agrupado = df_filtrado.groupby(valor).mean()
+        # Dejamos en el dataframe los artista que esta solo en el top 10
+        df_agrupado2 = df_agrupado[df_agrupado["ranking"] <= 10]
+        # Hacemos una grafica de barras horizontal. si quiere una grafica de barras vertical poner solo bar
+        df_agrupado2["ranking"].plot(kind="barh")
+        # Agragamos el titulo
+        if pais == "mx":
+            pais = "Mexico"
+        elif pais == "ec":
+            pais = "Ecuador"
+        elif pais == "pa":
+            pais = "Panama"
+        elif pais == "co":
+            pais = "Colombia"
+        elif pais == "hn":
+            pais = "Honduras"
+        elif pais == "ar":
+            pais = "Argentina"
+        elif pais == "gt":
+            pais = "Guatemala"
+        elif pais == "cr":
+            pais = "Costa Rica"
+        elif pais == "do":
+            pais = "Republica Dominicana"
+        elif pais == "es":
+            pais = "España"
+        elif pais == "cl":
+            pais = "Chile"
+
+        plt.title("Ranking 10: del 2017 al 2021 "+pais)
+        # Agregamos titulo de x
+        plt.xlabel("Ranking")
+        # Agregamos titulo de y
+        plt.ylabel(valor)
+        # Pintamos
+        plt.show()
+        #print(df_filtrado)
+
 analisis = analis_datos()
-analisis.ranking102017_2021("nombre")
+#analisis.ranking10_2017_2021("nombre")
+analisis.ranking10_2017_2021PorPais("cl","artista")
